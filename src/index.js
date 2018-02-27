229b6cde863446b2d12d484fcc2208eb5838bb7e
Ilya Fadzeyeu <Elias.Fadeyev@gmail.com>
2018-02-27 18:32:36 +0300

    longest-consective-sequence/feature: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  if (array.length === 0) return 0;
+  let max = array[0],
+  min = array[0],
+  obj = {},
+  countSequence = 1,
+  currentKey,
+  currentCount = 1;
+  array.forEach(element => {
+    if (element > max) max = element; 
+    else if (element < min) min = element;
+    obj[element] = true;
+  });
+  for (let key in obj) {
+    if (!currentKey) currentKey = key;
+    else if (key - currentKey === 1) currentCount++;
+    else {
+      if (currentCount > countSequence) countSequence = currentCount;
+      currentCount = 1;
+    }
+      currentKey = key;
+  }
+  return countSequence;
 }
